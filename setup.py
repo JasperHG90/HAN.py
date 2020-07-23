d9260c233871917a00b0323ad4217110328a59fa
Jasper Ginn <jasperginn@gmail.com>
2020-07-23 10:46:12 +0200

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="HAN",
-    version="0.1",
+    version="0.2",
     description="Train a Hierarchical Attention Model (HAN) on textual data.",
     url='https://github.com/FinTxt/HAN.py',
     author='Jasper Ginn',
